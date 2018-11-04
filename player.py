ec86f4404bb0ba3cae7dd5897814783b25775d29
nsklaus <nsklaus@yandex.com>
2018-11-04 22:41:07 +0100

    it doesn't stop, the brake, it doesn't do anything !

--- player.py
+++ player.py
@@ -5,7 +5,6 @@ from tkinter.filedialog import askopenfilenames
 from tkinter import ttk
 
 # TODO: make the player to automatically go to the next song
-# TODO: make double click on a song to start playing
 
 class App(object):
 
@@ -19,13 +18,16 @@ class App(object):
         self.style.theme_use('clam')
         self.root.title('mp3 player')
         self.parent_path = ""
+        # self.SONG_END = pygame.USEREVENT + 1
+        self.track_list = []
+        self.current_song = 0
 
         gui_style = ttk.Style()
         gui_style.configure('My.TFrame', background='#334353')
         self.frm = ttk.Frame(self.root, style='My.TFrame')
         self.frm.pack(anchor=N)
 
-        self.current_song = 0
+
 
         self.im_back = PhotoImage(file="images/back.png")
         self.im_play = PhotoImage(file="images/play.png")
@@ -71,31 +73,80 @@ class App(object):
         button_clr.pack(side=LEFT)
 
         self.listbox = Listbox(self.root)
+        self.listbox.bind('<Double-Button-1>', self.list_click)
         self.listbox.pack(fill=BOTH, expand=YES)
+
+    def play(self, trackn=None):
+        # if not trackn:  # case: no track was played before
+        #     trackn = self.listbox.curselection()  # try to see if something is selected in the list
+        #     if not trackn:  # case: nothing is selected in the list
+        #         my_string = self.listbox.get(0)
+        #         self.listbox.selection_clear(0, END)
+        #         self.listbox.selection_set(0, 0)
+        #     else:
+        #         my_string = self.listbox.get(trackn)
+        #     pygame.mixer.music.load(my_string)
+        # else:
+        #     my_string = self.listbox.get(trackn)
+        #     self.listbox.selection_clear(0, END)
+        #     self.listbox.selection_set(trackn, trackn)
+        #     pygame.mixer.music.load(my_string)
+        # print(self.listbox.curselection())
+        # for index, item in list(enumerate(self.listbox.get(self.listbox.curselection(), END))):
+        #     print("index = ", index, "item = ", self.listbox.get(index) )
+        #     pygame.mixer.music.load(item)
+        #     pygame.mixer.music.queue(item)
+        # print(type(self.listbox.get(self.listbox.curselection(), END)))
+        # pygame.mixer.music.load(self.listbox.get(index))
+
+        # pygame.mixer.music.play()
+        # while True:
+
+        # while pygame.mixer.music.get_busy():
+        #     pygame.time.Clock().tick(5)
+
+        song_end = pygame.USEREVENT + 1
+        pygame.mixer.music.set_endevent(song_end)
+        for event in pygame.event.get():
+            if event.type == song_end:
+                if not self.current_song >= len(self.track_list):
+                    self.current_song += 1
+                    print("the song ended!")
+
+        if len(list(self.listbox.get(0, END))) > 0:  # if we have a list of song
+            if not self.listbox.curselection():  # if no song is selected in the list
+                self.listbox.selection_set(0, 0)
+
+            if trackn is not None:
+                print("trackn = ", trackn)
+                print("len(list)= ", len(self.track_list))
+                print("trackn less than 0", trackn < 0)
+                print("trackn more or equal to len(list)", trackn >= len(self.track_list))
+                if not trackn < 0 and not trackn >= len(self.track_list):
+                    print("current_song before trackn ", self.current_song)
+                    pygame.mixer.music.stop()
+                    self.current_song = trackn
+                    print("current_song after trackn ", self.current_song)
+
+            # # self.track_list = list(self.listbox.get(self.listbox.curselection(), END))  # get list of song
+            # print("curr= ",self.current_song, "lentrack", len(self.track_list))
+            # print(self.track_list)
+            if not self.current_song >= len(self.track_list):
+                if not pygame.mixer.music.get_busy():
+                    self.listbox.selection_clear(0, END)
+                    self.listbox.selection_set(self.current_song, self.current_song)
+                    pygame.mixer.music.load(self.track_list[self.current_song])
+                    # print(self.track_list[self.current_song])
+                    pygame.mixer.music.play()
+
+        self.root.after(2000, self.play)
 
     def back(self):
         current = self.listbox.curselection()
-        prev = current[0] - 1
-        self.play(trackn=prev)
-        pass
-
-    def play(self, trackn=None):
-        if not trackn:  # case: no track was played before
-            trackn = self.listbox.curselection()  # try to see if something is selected in the list
-            if not trackn:  # case: nothing is selected in the list
-                my_string = self.listbox.get(0)
-                self.listbox.selection_clear(0, END)
-                self.listbox.selection_set(0, 0)
-            else:
-                my_string = self.listbox.get(trackn)
-            pygame.mixer.music.load(my_string)
-        else:
-            my_string = self.listbox.get(trackn)
-            self.listbox.selection_clear(0, END)
-            self.listbox.selection_set(trackn, trackn)
-            pygame.mixer.music.load(my_string)
-
-        pygame.mixer.music.play()
+        prev_track = current[0] - 1
+        # print("prev before = ", current[0])
+        # print("prev after = ", current[0] - 1)
+        self.play(trackn=prev_track)
 
     def next(self):
         current = self.listbox.curselection()
@@ -114,6 +165,7 @@ class App(object):
         for index, item in list(enumerate(self.frm.filename)):
             self.listbox.insert(END, self.frm.filename[index])
         self.parent_path = self.frm.filename[0].rsplit('/', 1)[0]  # remember last visited dir
+        self.track_list = list(self.listbox.get(0, END))  # get list of song
 
     def rem(self):
         current = self.listbox.curselection()
@@ -122,6 +174,12 @@ class App(object):
     def clear(self):
         self.listbox.delete(0, END)
 
+    def list_click(self, event):
+
+        current = self.listbox.curselection()
+        self.play(trackn=current[0])
+
 
 app = App()
+app.root.after(1000, app.play)
 app.root.mainloop()
